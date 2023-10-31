cb7ad1c66307894325cc8132e7e9adacd3423a7e
codeForMobile <code.mobile@protonmail.com>
2023-10-31 15:44:22 -0600

    type the query, generate db types and view data on ui

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,9 +1,11 @@
 import { gql, useQuery } from '@apollo/client'
 import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import { startCase, uniq } from 'lodash'
+import { SubmissionsQuery } from '../gql/graphql'
 
 const Dashboard = () => {
-  const { data, loading } = useQuery(gql`
-    query SubmissionsQuery {
+  const { data, error, loading } = useQuery<SubmissionsQuery>(gql`
+    query Submissions {
         submissions {
             id
             submittedAt
@@ -11,20 +13,28 @@ const Dashboard = () => {
         }
     }
   `)
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>Error:{error.message}</div>
   
+  // TODO: Type data for correct submissions type.
+  const { submissions } = data!
   const columns: GridColDef[] = [
     { field: 'id', headerName: 'id', width: 300},
     { field: 'submittedAt', headerName: 'submittedAt', width:200},
+    ...uniq(submissions.flatMap((s: any) => Object.keys(s.data))).map((field:string) => ({
+        field,
+        headerName: startCase(field)
+    }))
   ];
-  
-  if (loading) return <div>Loading...</div>
+
   return (
    // original version 
    //<div>{JSON.stringify(data)}</div>
    <div style={{ height: 300, width: '100%' }}>
     <DataGrid 
-        rows={data.submissions} 
-        columns={columns} 
+        rows={submissions}
+        columns={columns}
+        pageSize={5}
     />
     </div>
   )
