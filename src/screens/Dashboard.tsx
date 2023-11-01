e31214de4e95d10987b2b422c27d47607bb76a35
codeForMobile <code.mobile@protonmail.com>
2023-11-01 14:24:23 -0600

    add toolbar, button and enable queuing jobs with button

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,7 +1,31 @@
-import { gql, useQuery } from '@apollo/client'
-import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import { gql, useMutation, useQuery } from '@apollo/client'
+import styled from '@emotion/styled'
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
 import { startCase, uniq } from 'lodash'
 import { SubmissionsQuery } from '../gql/graphql'
+
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  flex-direction: column;
+`;
+
+const Toolbar = styled.div`
+  background: #eee;
+  display: flex;
+  justify-content: flex-end;
+  padding: 15px;
+`;
+
+const Button = styled.div`
+  background: black;
+  color: white;
+  padding: 10px;
+  border-radius: 8px;
+  border: none;
+`;
 
 const Dashboard = () => {
   const { data, error, loading } = useQuery<SubmissionsQuery>(gql`
@@ -13,6 +37,15 @@ const Dashboard = () => {
         }
     }
   `)
+
+
+  const [generateSubmissions] = useMutation(gql`
+    mutation GenerateSubmissions($count: Int!) {
+      queueSubmissionGeneration(count: $count)
+    }
+  `, {variables: {count:10}})
+
+
   if (loading) return <div>Loading...</div>
   if (error) return <div>Error:{error.message}</div>
   
@@ -23,20 +56,25 @@ const Dashboard = () => {
     { field: 'submittedAt', headerName: 'submittedAt', width:200},
     ...uniq(submissions.flatMap((s: any) => Object.keys(s.data))).map((field:string) => ({
         field,
-        headerName: startCase(field)
+        headerName: startCase(field),
+        width: 200,
+        valueGetter: (params: GridValueGetterParams) => params.row.data[field]
     }))
   ];
 
   return (
    // original version 
    //<div>{JSON.stringify(data)}</div>
-   <div style={{ height: 300, width: '100%' }}>
+    <Container>
+      <Toolbar>
+        <Button onClick={() => generateSubmissions()}>Generate Submissions</Button>
+      </Toolbar>
     <DataGrid 
         rows={submissions}
         columns={columns}
-        pageSize={5}
+        pageSize={15}
     />
-    </div>
+    </Container>
   )
 }
 
